6e75332ddffa1e60b06aecb3e2c79252116d85df
David Bing <davidb.devops@gmail.com>
2020-11-16 16:16:18 -0600

    update module: TaskDataProvider.js - add getTasks, useTasks and dispatchStateChangeEvent component Close #17

--- src/scripts/tasks/TaskDataProvider.js
+++ src/scripts/tasks/TaskDataProvider.js
@@ -3,6 +3,17 @@ author : davidb
 purpose: Provides CRUD operations against api/database.json API objects
 */
 
+// hang eventHub on top level index.html element so we hear all events
+const eventHub = document.querySelector(".container")
+
+// this function will be called after a task is saved/deleted to 
+// re-render the event list without having to refresh
+const dispatchStateChangeEvent = () => {
+    const eventStateChanged = new CustomEvent("taskStateChanged")
+
+    eventHub.dispatchEvent(taksStateChanged)
+}
+
 // init array to hold tasks returned by useTasks()
 let tasks = []
 
@@ -12,4 +23,17 @@ export const useTasks = () => {
 }
 
 // fetch & format data and populate tasks array
+export const getTasks = () => {
+    return fetch("http://localhost:8088/tasks")
+    .then(response => response.json() )
+    .then (
+        parsedTasks => {
+            console.table(parsedTasks)
+            tasks = parsedTasks
+        }
+    )
+}
+
+// post a new event object to the json file, invokes getTasks() and dispatchStateChangeEvent()
+
 
