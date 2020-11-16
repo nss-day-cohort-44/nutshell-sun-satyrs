d0acb1aa2d47e244ea5184de3bff4ccf661da2e8
Travis Stevenson <thetechtravis@outlook.com>
2020-11-16 16:02:41 -0600

    Added Dependency Graph.png and Dependency Graph #2.png files. Created users dictory. Created UserDataProvider.js module.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,14 @@
 import { LoginForm } from "./auth/LoginForm.js"
 import { RegisterForm } from "./auth/RegisterForm.js"
 import { Nutshell } from "./Nutshell.js"
-
+import { getUsers } from "./users/UserDataProvider.js"
 
 /*
-    1. Check if the user is authenticated by looking in session storage for `activeUser`
-    2. If so, render the Nutshell component
-    3. If not, render the login and registration forms
-    4. Also, if the user authenticates, and the login form is initially shown
-        ensure that the Nutshell component gets rendered
+1. Check if the user is authenticated by looking in session storage for `activeUser`
+2. If so, render the Nutshell component
+3. If not, render the login and registration forms
+4. Also, if the user authenticates, and the login form is initially shown
+ensure that the Nutshell component gets rendered
 */
+
+getUsers()
