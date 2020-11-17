667523c6c27cd2cd6b26f66782da673ce8644694
Sanjeet Prasad <sanjeet.l.prasad@gmail.com>
2020-11-17 14:39:17 -0600

    add the delete news fuction in the news data provider and in the add the eventlistner delete button id to delete the news from the dom

--- src/scripts/news/NewsDataProvider.js
+++ src/scripts/news/NewsDataProvider.js
@@ -37,3 +37,10 @@ export const saveNews = (news) => {
     .then(getNews)
     .then(dispatchStateChangeEvent)
 }
+
+export const deleteNews = (newsId) => {
+    return fetch(`http://localhost:8088/news/${newsId}`, {
+        method: "DELETE"
+    })
+        .then(getNews)
+}
