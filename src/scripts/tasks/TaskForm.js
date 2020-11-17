8113e06cbcd51d3152da5faade9e68287915fd2e
David Bing <davidb.devops@gmail.com>
2020-11-17 11:42:11 -0600

    update module: TaskForm.js - checkpoint taskform

--- src/scripts/tasks/TaskForm.js
+++ src/scripts/tasks/TaskForm.js
@@ -1,2 +1,20 @@
+/*
+author : davidb
+purpose: responsible for creating and rendering the task form
+*/
 // Then a form should be presented to the user with a field to enter in the task name
 // And a field to enter in the expected completion date
+
+// import components from external modules
+import { saveTask } from './TaskDataProvider.js'
+
+// hang eventHub on top-level main element in index.html
+const eventHub = document.querySelector('.container')
+
+// render form fields to the DOM
+
+
+
+
+
+// event listener to capture click events
