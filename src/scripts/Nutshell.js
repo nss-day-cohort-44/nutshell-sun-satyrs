d5d894e4ef2286f281a7dc2fb4926efd33783ee3
Travis Stevenson <thetechtravis@outlook.com>
2020-11-17 16:21:27 -0600

    Resolved conflicts

--- src/scripts/Nutshell.js
+++ src/scripts/Nutshell.js
@@ -1,20 +1,9 @@
-import { Message } from "./messages/Message.js"
 import { MessageForm } from "./messages/MessageForm.js"
 import { MessageList } from "./messages/MessageList.js"
-
-const contentTarget = document.querySelector(".container--left")
-
-export const Nutshell = () => {
-    // Render all your UI components here
-    MessageList()
-    MessageForm()
-import { getEvents, useEvents } from "./events/EventDataProvider.js"
 import { EventList } from "./events/EventList.js"
 import { NewsForm } from "./news/NewsForm.js"
 import { NewsList } from "./news/NewsList.js"
 import { taskButton } from './tasks/TaskButton.js'
-
-
 
 const contentTarget = document.querySelector(".welcome")
 
@@ -22,8 +11,10 @@ export const Nutshell = () => {
     // Render all your UI components here
     contentTarget.innerHTML += "<h1>Welcome to Nutshell!</h1>"
     //    
-   EventList()
-   taskButton()
-   NewsForm()
-   NewsList()
+    EventList()
+    taskButton()
+    NewsForm()
+    NewsList()
+    MessageList()
+    MessageForm()
 }
