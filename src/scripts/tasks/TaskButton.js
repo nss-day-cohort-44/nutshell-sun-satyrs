0d0761c845e17fd1becff40359b8464040c399b3
David Bing <davidb.devops@gmail.com>
2020-11-17 11:00:38 -0600

    new module: TaskButton.js - Implements taskButton component

---
+++ src/scripts/tasks/TaskButton.js
@@ -0,0 +1,28 @@
+// get a ref to the landing spot in index.html for the witnesses button
+const buttonTarget = document.querySelector("#tasks")
+
+// hang eventHub on top level container in index.html
+const eventHub = document.querySelector(".container")
+
+// render button to index.html
+export const taskButton = () => {
+    buttonTarget.innerHTML = `
+    <button id="display-task-button">Create New Task</button>
+   `
+}
+
+// listen for matching click event and create/dispatch CustomEvent: "createTaskClicked"
+eventHub.addEventListener("click", (clickEvent) => {
+    if (clickEvent.target.id === "display-task-button") {
+        // debug
+        console.log("Create New Task button was clicked")
+
+        // create event object 
+        const createTaskButtonClicked = new CustomEvent("createTaskClicked")
+
+        // what about detail: key/value pair
+
+        // dispatch event object
+        eventHub.dispatchEvent(createTaskButtonClicked)
+    }
+})
