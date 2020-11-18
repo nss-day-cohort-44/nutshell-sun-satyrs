9fdec7e3bc9157831cbeddb7691e4dfd42737123
David Bing <davidb.devops@gmail.com>
2020-11-18 10:40:22 -0600

    update module: TaskList.js - add TaskList() and render() components

--- src/scripts/tasks/TaskList.js
+++ src/scripts/tasks/TaskList.js
@@ -14,17 +14,48 @@
 // listeners - create task button to nutshell.js?
 
 // import external modules and components
-import { getTask, useTasks } from './TaskDataProvider.js'
-import { renderTaskForm } from './TaskDataProvider.js'
+import { getUsers, useUsers } from '../users/UserDataProvider.js'
+import { getTasks, useTasks } from './TaskDataProvider.js'
+// import { renderTaskForm } from './TaskDataProvider.js'
 import { TaskAsHTML } from './TaskHTML.js'
+import { taskButton } from './TaskButton.js'
 
 // define eventHub scope to be top level element in index.html
 const eventHub = document.querySelector(".container")
 
 // get a reference to the target HTML element
-const targetElement = document.querySelector("#task")
+const targetElement = document.querySelector("#tasks")
 
 // Listen for the custom event dispatched from
-eventHub.addEventListener("taskStateChanged", () => taskList() )
+eventHub.addEventListener("taskStateChanged", () => TaskList() )
 
 // Component state variables with initial values
+let tasks = []
+let users = []
+
+export const TaskList = () => {
+    getUsers()
+    .then(getTasks)
+    .then( () => {
+        tasks = useTasks()
+        users = useUsers()
+
+        render()
+    })
+}
+
+const render = () => {
+    let taskHTMLRep = ""
+
+    const activeUserTasks = tasks.filter(task => task.userId === parseInt(sessionStorage.getItem("activeUser")))
+    for ( const task of activeUserTasks) {
+        taskHTMLRep += TaskAsHTML(task)
+    }
+    const button = taskButton()
+    console.log(button)
+    targetElement.innerHTML = `
+    <h3>User Task</h3>
+        ${button}
+        ${taskHTMLRep}
+    `
+}
