df990b755424c9b06aaf2e1298a553355c5a90f6
David Bing <davidb.devops@gmail.com>
2020-11-18 10:55:38 -0600

    update module: TaskForm.js - update contentTarget and clickEventId target values

--- src/scripts/tasks/TaskForm.js
+++ src/scripts/tasks/TaskForm.js
@@ -12,7 +12,7 @@ purpose: responsible for creating and rendering the task form
 import { saveTask } from './TaskDataProvider.js'
 
 // get a reference to the target HTML element
-const contentTarget = document.querySelector("#tasks")
+const contentTarget = document.querySelector(".container--right")
 
 // define eventHub scope to be top level element in index.html
 const eventHub = document.querySelector('.container')
@@ -29,17 +29,17 @@ export const renderTaskForm = () => {
 }
 
 // event listener to capture click events
-eventHub.addEventListener("createTaskClicked", () => renderTaskForm() )
+// eventHub.addEventListener("createTaskClicked", () => renderTaskForm() )
 
 // listening for save button on form. storing new object with values. passing through save function
 eventHub.addEventListener("click", clickEvent => {
 
-    if(clickEvent.target.id === "create-task-button") {
+    if(clickEvent.target.id === "task--saveButton") {
         const userId = parseInt(sessionStorage.getItem("activeUser"))
         const task = document.querySelector("#task--name").value
         const dueDate = document.querySelector("#task--dueDate").value
         const complete = false
-
+        
         const newTask = {
             userId,
             task,
